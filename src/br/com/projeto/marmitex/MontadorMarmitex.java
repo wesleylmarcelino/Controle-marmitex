e3bf1be9e49eb52dfe6b690db1230d32d6ae9f07
wesleylmarcelino <72048448+wesleylmarcelino@users.noreply.github.com>
2022-01-24 22:24:36 -0300

    Implementação da classe filha MontadorMarmitex

---
+++ src/br/com/projeto/marmitex/MontadorMarmitex.java
@@ -0,0 +1,16 @@
+package br.com.projeto.marmitex;
+
+public class MontadorMarmitex extends Funcionario {
+
+	public MontadorMarmitex(String nome, String cpf, int qtdFilhos) {
+		super(nome, cpf, qtdFilhos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double salarioComBonificacaoQtdFilhos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
